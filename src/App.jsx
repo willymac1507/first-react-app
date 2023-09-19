8a70675a0285d3f7703aa3e212283f58a5bc23b2
Will McCloy <william.mccloy@googlemail.com>
2023-09-19 11:55:52 +0100

    Swap to VSC

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="text-2xl font-bold underline">Help</p>
         <a
           className="App-link"
           href="https://reactjs.org"
