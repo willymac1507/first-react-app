05c996ef3afb611861c44c0fe471d42ebeb22ffe
Will McCloy <william.mccloy@googlemail.com>
2023-09-19 15:03:23 +0100

    End Episode 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,38 +6,38 @@ import Another from "./NewComponent";
 function App() {
 	const [count, setCount] = useState(0);
 	function decrement() {
-		setCount((prevCount) => prevCount - 1);
+		setCount(prevCount => prevCount - 1);
 	}
 
 	function increment() {
-		setCount((prevCount) => prevCount + 1);
+		setCount(prevCount => prevCount + 1);
 	}
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<Another name='Will' />
-				<img src={logo} className='App-logo' alt='logo' />
+		<div className="App">
+			<header className="App-header">
+				<Another name="Will" />
+				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
-				<div className='text-2xl font-bold'>
+				<div className="text-2xl">
 					<span>{count}</span>
 					<button
-						className='border bg-white text-black mx-3 px-3 py-1 rounded-xl'
+						className="border bg-white text-black mx-3 px-3 py-1 rounded-xl"
 						onClick={decrement}>
 						-
 					</button>
 					<button
-						className='border bg-white text-black mx-3 px-3 py-1 rounded-xl'
+						className="border bg-white text-black mx-3 px-3 py-1 rounded-xl"
 						onClick={increment}>
 						+
 					</button>
 				</div>
 				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'>
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer">
 					Learn React
 				</a>
 			</header>
