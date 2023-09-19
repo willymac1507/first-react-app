290c4cd58fdf096d993dab3df66335686a78bb5f
Will McCloy <william.mccloy@googlemail.com>
2023-09-19 12:45:53 +0100

    End Ep 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Another from "./NewComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="text-2xl font-bold underline">Help</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+	function decrement() {
+		setCount((prevCount) => prevCount - 1);
+	}
+
+	function increment() {
+		setCount((prevCount) => prevCount + 1);
+	}
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<Another name='Will' />
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<div className='text-2xl font-bold'>
+					<span>{count}</span>
+					<button
+						className='border bg-white text-black mx-3 px-3 py-1 rounded-xl'
+						onClick={decrement}>
+						-
+					</button>
+					<button
+						className='border bg-white text-black mx-3 px-3 py-1 rounded-xl'
+						onClick={increment}>
+						+
+					</button>
+				</div>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
